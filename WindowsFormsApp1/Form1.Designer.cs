08422266629cbc68d52e56a380f58018a96012b8
Michael <michaelshenouda5@gmail.com>
2021-08-15 01:37:43 +0200

    importing project

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_mn = new System.Windows.Forms.TextBox();
+            this.txt_nationality = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(116, 249);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 0;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(252, 249);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 1;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "الإسم";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "الجنسية";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "رقم الموبايل";
+            // 
+            // txt_mn
+            // 
+            this.txt_mn.Location = new System.Drawing.Point(70, 145);
+            this.txt_mn.Name = "txt_mn";
+            this.txt_mn.Size = new System.Drawing.Size(153, 20);
+            this.txt_mn.TabIndex = 7;
+            // 
+            // txt_nationality
+            // 
+            this.txt_nationality.Location = new System.Drawing.Point(70, 96);
+            this.txt_nationality.Name = "txt_nationality";
+            this.txt_nationality.Size = new System.Drawing.Size(153, 20);
+            this.txt_nationality.TabIndex = 8;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(70, 50);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(153, 20);
+            this.txt_name.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 330);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_nationality);
+            this.Controls.Add(this.txt_mn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_save);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_mn;
+        private System.Windows.Forms.TextBox txt_nationality;
+        private System.Windows.Forms.TextBox txt_name;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MIC;Initial Catalog=TestDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO insert_data(name, nationality, mb_no) VALUES ('"+txt_name.Text+"', '"+txt_nationality.Text+ "', '" + txt_mn.Text + "')", con);
+            int i = cmd.ExecuteNonQuery();
+            if (i != 0)
+            {
+                MessageBox.Show("saved");
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
+
+                con.Close();
+
+        }
+
+    }
+}
